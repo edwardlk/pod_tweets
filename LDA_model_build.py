72c762534af2862a9a18757ef281648bac997cd8
Ed K <6785562+edwardlk@users.noreply.github.com>
2020-02-11 12:54:40 -0500

    calc topic ranking & save

--- LDA_model_build.py
+++ LDA_model_build.py
@@ -3,8 +3,8 @@ import re
 import sklearn
 import regex
 import emoji
+import pandas as pd
 import preprocessor as p
-from sklearn import datasets
 from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
 from sklearn.decomposition import NMF, LatentDirichletAllocation
 from nltk.stem import WordNetLemmatizer, SnowballStemmer
@@ -116,10 +116,11 @@ def print_top_words(model, feature_names, n_top_words):
 data_dir = './follower_twts/'
 resources_dir = './resources/'
 categories = os.listdir(data_dir)
-print(categories)
 
 have_file_dump = True
-file_dump_name = '202002051521_tweet_p_list.joblib'
+have_model_dump = True
+file_dump_name = '202002102056_tweet_p_list.joblib'
+model_dump = '202002110724_LDA_Grid.joblib'
 
 if not have_file_dump:
     print('Loading dataset...', end='')
@@ -137,8 +138,10 @@ if not have_file_dump:
         tweet_p_list.extend(sublist)
     print('Done')
 
+    print('Dumping formatted tweets...', end='')
     dump1 = (resources_dir + '{}_tweet_p_list.joblib'.format(datetime.now().strftime("%Y%m%d%H%M")))
     dump(tweet_p_list, dump1)
+    print('Done')
 else:
     load_file = resources_dir + file_dump_name
     tweet_p_list = load(load_file)
@@ -151,24 +154,28 @@ tf_vectorizer = CountVectorizer(
 tf = tf_vectorizer.fit_transform(tweet_p_list)
 print('Done')
 
-# Define Search Param
-search_params = {'n_components': [10], 'learning_decay': [0.5]}
+if not have_model_dump:
+    # Define Search Param
+    search_params = {'n_components': [40], 'learning_decay': [0.9]}
 
-# Init the Model
-lda = LatentDirichletAllocation(
-    n_components=n_components, max_iter=5, learning_method='online',
-    learning_offset=50., random_state=0, n_jobs=-1)
+    # Init the Model
+    lda = LatentDirichletAllocation(
+        n_components=n_components, max_iter=5, learning_method='online',
+        learning_offset=50., random_state=0, n_jobs=-1)
 
-# Init Grid Search Class
-model = GridSearchCV(lda, param_grid=search_params, n_jobs=-1, verbose=2)
+    # Init Grid Search Class
+    model = GridSearchCV(lda, param_grid=search_params, n_jobs=-1, verbose=2)
 
-# Do the Grid Search
-print('Beginning the grid search...', end='')
-model.fit(tf)
-print('Done')
+    # Do the Grid Search
+    print('Beginning the grid search...', end='')
+    model.fit(tf)
+    print('Done')
 
-dump2 = (resources_dir + '{}_LDA_Grid.joblib'.format(datetime.now().strftime("%Y%m%d%H%M")))
-dump(model, dump2)
+    dump2 = (resources_dir + '{}_LDA_Grid.joblib'.format(datetime.now().strftime("%Y%m%d%H%M")))
+    dump(model, dump2)
+else:
+    model_file = resources_dir + model_dump
+    model = load(model_file)
 
 # Best Model
 best_lda_model = model.best_estimator_
@@ -181,3 +188,46 @@ print("Best Log Likelihood Score: ", model.best_score_)
 
 # Perplexity
 print("Model Perplexity: ", best_lda_model.perplexity(tf))
+
+topic_names = ["Topic" + str(i) for i in range(best_lda_model.n_components)]
+topic_matrix = pd.DataFrame(columns=topic_names)
+
+categories_list = docs_to_train.target_names
+
+for x1 in range(len(categories_list)):
+    subdocs_to_train = sklearn.datasets.load_files(
+        data_dir, description=None, categories=categories_list[x1],
+        load_content=True, encoding='utf-8', shuffle=True, random_state=42)
+
+    subset_tweet_txt = subdocs_to_train['data']
+
+    print("Extracting tf features for LDA of {}...".format(categories_list[x1]))
+    sub_tf_vectorizer = CountVectorizer(
+        token_pattern=r'[^\s]+', encoding='unicode', lowercase=None, strip_accents=None, stop_words=None,
+        max_features=n_features)
+
+    reformat = lambda x: tweet_split_2_process(x)
+    subset_tweet_txt_p = list(map(reformat, subset_tweet_txt))
+    subset_tweet_p_list = []
+
+    for sublist in subset_tweet_txt_p:
+        subset_tweet_p_list.extend(sublist)
+
+    corp_to_tf = [' '.join(subset_tweet_p_list)]
+
+    sub_tf = sub_tf_vectorizer.fit_transform(corp_to_tf)
+
+    X = best_lda_model.transform(sub_tf)
+
+    topic_matrix.loc[categories_list[x1]] = X[0]
+
+topic_matrix.to_pickle(resources_dir + 'topic_matrix_{}.pkl'.format(datetime.now().strftime("%Y%m%d%H%M")))
+
+
+rank_matrix = pd.DataFrame()
+for x2 in range(len(topic_matrix)):
+    df = pd.DataFrame(topic_matrix.iloc[x2])
+    df = df.sort_values(by=[str(df.columns[0])], ascending=False)
+    rank_matrix[df.columns[0]] = df.index.tolist()
+
+rank_matrix.to_pickle(resources_dir + 'rank_matrix_{}.pkl'.format(datetime.now().strftime("%Y%m%d%H%M")))
